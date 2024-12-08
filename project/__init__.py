d1dc49e9796c46e7b70369eeef322d2be1789ea6
Nier'seDev <nienachuraru@yahoo.com>
2024-12-08 11:38:59 +0800

    Created Project File

---
+++ project/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+"""
+URL Routes:
+    Primary:
+    Home (Logged Out) / Dashboard (Logged In)
+
+    Secondary:
+    Courses (Logged In) / Forum (Logged Out / Logged In) / ARReS
+"""
+@app.route('/')
+def home():
+    return render_template("home.html")
