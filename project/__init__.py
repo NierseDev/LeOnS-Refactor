f2980bcd1859e1f713590e75e2d0ccf9ad7e8aec
Nier'seDev <nienachuraru@yahoo.com>
2024-12-13 10:40:55 +0800

    updated navbar

--- project/__init__.py
+++ project/__init__.py
@@ -14,3 +14,27 @@ URL Routes:
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/courses')
+def courses():
+    return render_template("courses.html")
+
+@app.route('/')
+def course_post():
+    return render_template("course_post.html")
+
+@app.route('/forum')
+def forum():
+    return render_template("forum.html")
+
+@app.route('/')
+def forum_post():
+    return render_template("forum_post.html")
